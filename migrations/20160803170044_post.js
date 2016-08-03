bcfb6979c7cdb7c261b95ee29f678e0a78c24962
EDA <eda@mac-mini-3.local>
2016-08-03 18:10:47 +1200

    post migration schema filled

--- migrations/20160803170044_post.js
+++ migrations/20160803170044_post.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('posts', function (table) {
+    table.increments('id').primary()
+    table.string('title')
+    table.string('content')
+    table.integer('user_id').references()
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('posts')
 };
